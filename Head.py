b0b6b530b31000ebb68d0ca1bfca4e1662a17ba8
侯程 <nnhoucheng@172-16-21-5.DYNAPOOL.NYU.EDU>
2017-06-09 14:25:12 -0400

    Head.py

--- Head.py
+++ Head.py
@@ -3,12 +3,10 @@ import sys
 
 if __name__ == '__main__':
     sc = SparkContext()
-    if len(sys.argv) == 4:
-        with open('path.txt', 'r') as fi:
-            files = fi.readlines()
-        path = files[0] + files[sys.argv[1]]
+    if len(sys.argv) == 2:        
+        path = '/gws/projects/project-taxi_capstone_2016/data/TLC/TPEP2015/BreadCrumb_CMT.csv'
         f = sc.textFile(path, use_unicode=False).cache()
-        head = f.select(sys.argv[3])
-        with open(sys.argv[2], 'wb') as fo:
+        head = f.select(10)
+        with open(sys.argv[1], 'wb') as fo:
             for h in head:
                 fo.write('%s'%h)
