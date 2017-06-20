d936e52654b5bca58aaff22b72b35bdbc0c6218d
侯程 <nnhoucheng@172-16-20-205.DYNAPOOL.NYU.EDU>
2017-06-20 15:49:13 -0400

    Head.py

--- Head.py
+++ Head.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:        
         path = '/gws/projects/project-taxi_capstone_2016/data/TLC/TPEP2015/BreadCrumb_CMT.csv'
         f = sc.textFile(path, use_unicode=False).cache()
-        head = f.select(10)
+        head = f.take(10)
         with open(sys.argv[1], 'wb') as fo:
             for h in head:
                 fo.write('%s'%h)
