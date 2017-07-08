0719e5b66155fc23850153f1f69ad63eb646449e
侯程 <nnhoucheng@172-16-194-248.DYNAPOOL.NYU.EDU>
2017-07-08 17:31:15 -0400

    OneYear.py

--- OneYear.py
+++ OneYear.py
@@ -7,13 +7,35 @@ import matplotlib.pyplot as plt
 import geopy.distance
 from datetime import datetime
 from scipy.ndimage.interpolation import shift
-
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 
 if __name__ == '__main__':
     sc = SparkContext()
+    
+    # Step 0: Read Data
+    ## Breagcrumb Data: 5 columns
+    ### 0: shl_number or medallion
+    ### 1: timestamp in yymmddhhmmss
+    ### 2: longitude in crs 2263
+    ### 3: latitude in crs 2263
+    ### 4: number of passengers in taxi
     path = '/gws/projects/project-taxi_capstone_2016/data/breadcrumb_nad83'
-    f = sc.textFile(path, use_unicode=False).cache()
-    head = f.take(10)
-    rdd = sc.parallelize(head)
+    Breadcrumb = sc.textFile(path, use_unicode=False).cache()
+    
+    # Step 1: Filter out customer trip and time period
+    def parseCSV(list_of_records):
+        import csv
+        reader = csv.reader(list_of_records)
+        for row in reader:
+            ## Filter out N/A
+            if len(row) == 5:
+                ## Filter out customer trip
+                if row[-1] == '0':
+                    ## from '2015-01-01' to '2015-01-31'
+                    if row[1][:4] == '1501'
+                        yield (row[0], (row[1], row[2], row[3]))
+            
+    BC = Breadcrumb.mapPartitions(parseCSV)
+    rdd = sc.parallelize(BC.take(10))
     rdd.saveAsTextFile('tmp')
