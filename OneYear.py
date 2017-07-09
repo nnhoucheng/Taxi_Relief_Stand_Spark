0c17b245b25d037514edd880ea5ef7cf7d0dfb2c
侯程 <nnhoucheng@172-16-194-248.DYNAPOOL.NYU.EDU>
2017-07-09 15:02:56 -0400

    OneYear.py

--- OneYear.py
+++ OneYear.py
@@ -33,9 +33,65 @@ if __name__ == '__main__':
                 ## Filter out customer trip
                 if row[-1] == '0':
                     ## from '2015-01-01' to '2015-01-31'
-                    if row[1][:4] == '1501'
+                    if row[1][:4] == '1501':
                         yield (row[0], (row[1], row[2], row[3]))
             
     BC = Breadcrumb.mapPartitions(parseCSV)
-    rdd = sc.parallelize(BC.take(10))
-    rdd.saveAsTextFile('tmp')
+    
+    # Step 2: Find idle points
+    def findidle(values):
+        import numpy as np
+        ## Transform timestamp to seconds
+        def trans(timestamp):
+            return int(timestamp[:2])*3600 + int(timestamp[2:4])*60 + int(timestamp[4:])
+        
+        ## After grouping by (vehicle, day), extract and sort values by time
+        rows = []
+        for v in values:
+            rows.append([trans(v[0]), int(v[1]), int(v[2])]) 
+        rows = np.array(sorted(rows, key=lambda x:x[0]))
+        
+        ## Check if the speed within the threshold
+        def checkspeed(row1, row2, speed_threshold = 10*3.28084/120):
+            speed = 1.*np.sqrt((row1[1]-row2[1])**2 + (row1[2]-row2[2])**2) / (row2[0]-row1[0])
+            if speed <= speed_threshold:
+                return True
+            else:
+                return False
+
+        idles = []
+        time_thresold = 10*60 # time thershold
+        flag = False
+        start = 0
+        for i,row in enumerate(rows):
+            if i == 0:
+                continue
+            if checkspeed(rows[i-1], row) and not flag:
+                flag = True
+                start = i-1
+            elif not checkspeed(rows[i-1], row) and flag:
+                flag = False
+                time = rows[i-1][0] - rows[start][0]
+                if time >= time_thresold:
+                    idles.append((np.mean(rows[start:i,1]), np.mean(rows[start:i,2]), rows[start][0], time))
+        return (idles)
+    
+    Idles = BC.groupByKey().mapValues(findidle)
+    
+    # Step 3: Save idle points
+    def mapback(list_of_records):
+        for kvs in list_of_records:
+            k, vs = kvs
+            output = k[0] + ',' + k[1]
+            def trans_(time):
+                hour = time//3600
+                time = time%3600
+                minute = time//60
+                second = time%60
+                return ('0'+str(hour))[-2:] + ('0'+str(minute))[-2:] + ('0'+str(second))[-2:]
+            for v in vs:
+                yield output + trans_(v[2]) + ',' + str(v[3]) + ',' + str(int(round(v[0]))) + ',' + str(int(round(v[1])))
+                
+    Idles.mapPartitions(mapback).saveAsTextFile('tmp')
+    
+    
