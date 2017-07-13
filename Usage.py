d1864d65af41f32fb921ddfa73aec481ab24885e
侯程 <nnhoucheng@nnhc-Air.fios-router.home>
2017-07-12 21:16:44 -0400

    Usage.py

--- Usage.py
+++ Usage.py
@@ -8,7 +8,7 @@ from fiona.crs import from_epsg
 def parse(records):
     reader = csv.reader(records)
     
-    relief_path = 'new_york_city_taxi_relief_stations.geojson'
+    relief_path = '/user/ch3019/capstone/new_york_city_taxi_relief_stations.geojson'
     relief = gpd.GeoDataFrame.from_file(relief_path)
     relief.crs = from_epsg(4326)
     relief = relief.to_crs(epsg=2263)
