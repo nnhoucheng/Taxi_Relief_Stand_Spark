46020bf58206a86db5cef06ef86b7aae6089adbe
侯程 <nnhoucheng@nnhc-Air.fios-router.home>
2017-07-19 12:42:07 -0400

    Head.py

--- Head.py
+++ Head.py
@@ -2,10 +2,7 @@ from pyspark import SparkContext
 import sys
 
 if __name__ == '__main__':
-    sc = SparkContext()
-    if len(sys.argv) == 2:        
-        path = '/gws/projects/project-taxi_capstone_2016/data/TLC/TPEP2015/BreadCrumb_CMT.csv'
-        f = sc.textFile(path, use_unicode=False).cache()
-        head = f.take(10)
-        rdd = sc.parallelize(head)
-        rdd.saveAsTextFile('tmp')
+    sc = SparkContext()        
+    head = sys.argv
+    rdd = sc.parallelize(head)
+    rdd.saveAsTextFile('tmp')
