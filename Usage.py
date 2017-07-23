6709350016880900f6614e3c75e228ee3da35a16
侯程 <nnhoucheng@172-16-16-33.DYNAPOOL.NYU.EDU>
2017-07-23 12:42:11 -0400

    Usage.py

--- Usage.py
+++ Usage.py
@@ -11,7 +11,8 @@ def parse(records):
     
     distance = 1/.3048*50 # setup the distance parameter
     #relief_path = 'new_york_city_taxi_relief_stations.geojson'
-    relief_path = 'relief_stands_23July.geojson'
+    #relief_path = 'relief_stands_23July.geojson'
+    relief_path = 'TRS_149.geojson'
     relief = gpd.GeoDataFrame.from_file(relief_path)
     relief.crs = from_epsg(4326)
     relief = relief.to_crs(epsg=2263)
